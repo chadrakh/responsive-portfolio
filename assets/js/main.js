054a5687be46d14114dac950f6aa2d59e3f0983f
chadrakh <89146273+chadrakh@users.noreply.github.com>
2021-08-18 23:41:49 +0100

    HTML Ext page buttons
    LinkedIn, Dribble, GitHub link buttons home page image, intro line

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,11 +15,14 @@ showMenu('nav-toggle','nav-menu')
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
+    // Active link
     navLink.forEach(n => n.classList.remove('active'))
     this.classList.add('active')
-}
-
-navLink.forEach(n => n.addEventListener('click', linkAction))
+    // Remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+  }
+  navLink.forEach(n => n.addEventListener('click', linkAction));
 
   /*Active link*/
 
