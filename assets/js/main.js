2177a5231abfe0049bf2e0aac8ce1ec2229cd11c
chadrakh <89146273+chadrakh@users.noreply.github.com>
2021-08-18 20:34:13 +0100

    Active and Remove menu in JS
    Menu sections underlined when selected and hovered over. JavaScript coded for Menu Show & Active and Remove Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,14 @@ const showMenu = (toggleId, navId) =>{
 showMenu('nav-toggle','nav-menu')
 
 /*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
   /*Active link*/
 
